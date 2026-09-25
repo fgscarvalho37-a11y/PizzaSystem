90383a8bd70abafcc458c083f49d306a26276156
Felipe Carvalho | Backend Developer <fgscarvalho37@gmail.com>
2026-09-25 00:27:03 -0300

    refactor: use same-origin API in storefront

--- frontend/src/app/pagamento/[id]/page.tsx
+++ frontend/src/app/pagamento/[id]/page.tsx
@@ -3,9 +3,7 @@
 import { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
 
-const API_URL =
-  process.env.NEXT_PUBLIC_API_URL ??
-  "http://localhost:8080";
+const API_URL = "";
 
 type PixPayment = {
   id: string;
